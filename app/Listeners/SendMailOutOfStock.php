4f0e008a5dbc2dd2d2b3dd5d97a3c2a8650cab61
Eslam Nabil <eslamnabilmohamed2@gmail.com>
2022-09-26 08:33:57 +0200

    create lisnter

---
+++ app/Listeners/SendMailOutOfStock.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ProductOutOfStock;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendMailOutOfStock
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\ProductOutOfStock  $event
+     * @return void
+     */
+    public function handle(ProductOutOfStock $event)
+    {
+        //
+    }
+}
